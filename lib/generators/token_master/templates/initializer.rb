241a07ec01081c6211e30838d55ae533c30cbeb0
Ifat <iribon9@gmail.com>
2017-03-16 09:30:07 -0500

    update initializer file

--- lib/generators/token_master/templates/initializer.rb
+++ lib/generators/token_master/templates/initializer.rb
@@ -1,23 +1,24 @@
 TokenMaster.config do |config|
-
-  # The number of days the token for each tokenable is active
-  # default:
-  # config.token_lifetimes = {
-  #   confirm: 14,
-  #   reset: 1,
-  #   invite: 10
-  # }
-
   # The number of characters in a token
   # default: 20
-  #
+
   # config.token_length = 30
 
-  # Required parameters for tokenable actions
+  # Options for each tokenable
   # default:
 
-  # config.required_params = {
-  #   reset: [:password, :password_confirmation],
-  #   invite: [:password, :password_confirmation]
+  # config.confirm_options = {
+  #   token_lifetime: 14,
+  #   required_parms: []
+  # }
+
+  # config.reset_options = {
+  #   token_lifetime: 1,
+  #   required_parms: [:password, :password_confirmation]
+  # }
+
+  # config.invite_options = {
+  #   token_lifetime: 10,
+  #   required_parms: [:password, :password_confirmation]
   # }
 end
