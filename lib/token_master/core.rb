d82f005a1d3668204bcc7e6e87fad831917f8296
Ifat <iribon9@gmail.com>
2017-03-16 09:27:29 -0500

    added status methods

--- lib/token_master/core.rb
+++ lib/token_master/core.rb
@@ -8,14 +8,14 @@ module TokenMaster
       def token_master(*tokenables)
         tokenables.each do |tokenable|
           #instance methods
-          define_method("set_#{tokenable}_token".to_s) do
+          define_method("set_#{tokenable}_token".to_sym) do
             TokenMaster.set_token!(self, tokenable)
           end
-          define_method("send_#{tokenable}_instruction".to_s) do
+          define_method("send_#{tokenable}_instruction".to_sym) do
             TokenMaster.send_instructions!(self, tokenable)
           end
-          define_method("#{tokenable}_pending?".to_s) do
-            TokenMaster.pending?(self, tokenable)
+          define_method(:status) do
+            TokenMaster.status(self, tokenable)
           end
           #class methods
           define_method("self.#{tokenable}_by_token".to_s) do |token, **params|
