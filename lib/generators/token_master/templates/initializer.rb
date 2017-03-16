121e6e9a3cb7fb21623cb089192bb93c22a5ee65
Ifat <iribon9@gmail.com>
2017-03-16 14:42:50 -0500

    updated initializer with help text

--- lib/generators/token_master/templates/initializer.rb
+++ lib/generators/token_master/templates/initializer.rb
@@ -1,19 +1,31 @@
 TokenMaster.config do |config|
-  # Options for each tokenable
-  # default:
+  # Set up your configurations for each tokenable using the methods at the bottom of this file.
+  # Examples:
+
+  # 'config.add_tokenable_options :confirm, TokenMaster::Config::DEFAULT_VALUES' results in:
 
   # config.confirm_options = {
   #   token_lifetime: 14,
-  #   required_parms: []
+  #   required_parms: [],
+  #   token_length: 20
   # }
+
+  # 'config.add_tokenable_options :reset, token_lifetime: 1, required_params: [:password, :password_confirmation], token_length: 15' results in:
 
   # config.reset_options = {
   #   token_lifetime: 1,
-  #   required_parms: [:password, :password_confirmation]
+  #   required_parms: [:password, :password_confirmation],
+  #   token_length: 20
   # }
 
-  # config.invite_options = {
+  # 'config.add_tokenable_options :foo, token_lifetime: 10, required_params: [:email, token_length: config.DEFAULT_VALUES[:token_length]' results in:
+
+  # config.foo_options = {
   #   token_lifetime: 10,
-  #   required_parms: [:password, :password_confirmation]
+  #   required_parms: [:email],
+  #   token_length: 20
   # }
+
+
+  #### METHODS FOR YOUR CONFIGURATION BELOW ###
 end
