e526d1cc13e76ab3a2e1d44c724b3a04ab42ef62
Ifat <iribon9@gmail.com>
2017-03-28 20:15:11 -0500

    changed to migration generator

--- lib/generators/token_master/token_master_generator.rb
+++ lib/generators/token_master/token_master_generator.rb
@@ -2,13 +2,11 @@ require 'rails/generators/named_base'
 
 module TokenMaster
   module Generators
-    class TokenMasterGenerator < Rails::Generators::NamedBase
-
-      namespace "token_master"
+    class MigrationGenerator < Rails::Generators::NamedBase
+      namespace 'token_master'
       source_route File.expand_path('../templates', __FILE__)
 
       desc 'Generates a migration file for the given model (NAME) with the specified tokenable columns'
-
     end
   end
 end
