7f1ecedfdbc87fbe0aa2d9119aa067d78b215e70
Ifat <iribon9@gmail.com>
2017-03-28 20:34:27 -0500

    fixed typo

--- lib/generators/token_master/migration_generator.rb
+++ lib/generators/token_master/migration_generator.rb
@@ -5,7 +5,7 @@ module TokenMaster
   module Generators
     class MigrationGenerator < Rails::Generators::NamedBase
       namespace 'token_master'
-      source_route File.expand_path('../templates', __FILE__)
+      source_root File.expand_path('../templates', __FILE__)
 
       desc 'Generates a migration file for the given model (NAME) with the specified tokenable columns'
     end
