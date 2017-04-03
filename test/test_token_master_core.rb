645bd17ce28013caa72f54b87bb1dcfaedeaa307
Ifat <iribon9@gmail.com>
2017-04-03 17:42:10 -0500

    added core specs

--- test/test_token_master_core.rb
+++ test/test_token_master_core.rb
@@ -75,9 +75,19 @@ end
 
 # TM = TokenMaster::Model
 describe TokenMaster::Core do
+  # before(:all) do
+  #   class User
+  #     include TokenMaster::Core
+  #     token_master :confirm
+  #   end
+  #   @foo = User
+  # end
   before do
-    @tokenable_model = MockTokenMaster.new
-    @klass = MockTokenMaster
+    @bar = Class.new do |incl|
+      incl.include TokenMaster::Core
+      incl.token_master :confirm
+    end
+    @tokenable_model = @bar.new
     TokenMaster.config do |config|
       config.add_tokenable_options(:confirm, TokenMaster::Config::DEFAULT_VALUES)
       config.add_tokenable_options(:reset,
@@ -89,15 +99,53 @@ describe TokenMaster::Core do
 
   describe 'sets methods' do
     it 'instance methods' do
-      assert_send([@tokenable_model, :set_confirm_token!])
-      assert_send([@tokenable_model, :send_confirm_instructions!])
-      assert_send([@tokenable_model, :confirm_status])
-      assert_send([@tokenable_model, :force_confirm!])
+      # user = @foo.new
+
+      # assert_send([@tokenable_model, :set_confirm_token!])
+      # assert_send([@tokenable_model, :send_confirm_instructions!])
+      # assert_send([@tokenable_model, :confirm_status])
+      # assert_send([@tokenable_model, :force_confirm!])
     end
 
     it 'class methods' do
-      token = 'foo'
-      assert_send([@klass, :confirm_by_token!, token])
+      assert_respond_to(@bar, :confirm_by_token!)
+    end
+  end
+
+  describe 'calls methods correctly' do
+    describe 'class methods' do
+      it '#do_by_token' do
+        token = 'foo'
+        TokenMaster::Model.stub :do_by_token!, :foo do
+          assert_equal @bar.confirm_by_token!(token), :foo
+        end
+      end
+    end
+
+    describe 'instance methods' do
+      it '#set_token!' do
+        TokenMaster::Model.stub :set_token!, :foo do
+          assert_equal @tokenable_model.set_confirm_token!, :foo
+        end
+      end
+
+      it '#send_instructions!' do
+        TokenMaster::Model.stub :send_instructions!, :foo do
+          assert_equal @tokenable_model.send_confirm_instructions!, :foo
+        end
+      end
+
+      it '#status' do
+        TokenMaster::Model.stub :status, :foo do
+          assert_equal @tokenable_model.confirm_status, :foo
+        end
+      end
+
+      it '#force_tokenable!' do
+        TokenMaster::Model.stub :force_tokenable!, :foo do
+          assert_equal @tokenable_model.force_confirm!, :foo
+        end
+      end
     end
   end
 end
