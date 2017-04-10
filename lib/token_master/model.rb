12f0c7d402a9ef561f4241de7c95fc07d3c47de5
David Corwin <dave@launchpadlab.com>
2017-04-10 14:38:58 -0500

    add todos

--- lib/token_master/model.rb
+++ lib/token_master/model.rb
@@ -6,19 +6,18 @@ module TokenMaster
       base.extend(ClassMethods)
     end
 
-    # TODO
     module ClassMethods
       # TODO
       def token_master(*tokenables)
         tokenables.each do |tokenable|
           # instance methods
           # TODO
-          define_method("set_#{tokenable}_token!".to_sym) do
+          define_method("set_#{tokenable}_token!") do
             TokenMaster::Core.set_token!(self, tokenable)
           end
           # TODO
-          define_method("send_#{tokenable}_instructions!".to_sym) do |&email|
-            TokenMaster::Core.send_instructions!(self, tokenable, &email)
+          define_method("send_#{tokenable}_instructions!") do |email|
+            TokenMaster::Core.send_instructions!(self, tokenable, email)
           end
           # TODO
           define_method("#{tokenable}_status") do
@@ -30,7 +29,7 @@ module TokenMaster
           end
           # class methods
           # TODO
-          define_singleton_method("#{tokenable}_by_token!".to_sym) do |token, **params|
+          define_singleton_method("#{tokenable}_by_token!") do |token, **params|
             TokenMaster::Core.do_by_token!(self, tokenable, token, **params)
           end
         end
