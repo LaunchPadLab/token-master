830c26ab1d7aa8e1e2b5fc968328bc31e2e30252
David Corwin <dave@launchpadlab.com>
2017-04-18 11:36:18 -0500

    rename test files, fix test

--- test/test_model.rb
+++ test/test_model.rb
@@ -38,7 +38,7 @@ describe TokenMaster::Model do
 
       it '#send_instructions!' do
         TokenMaster::Core.stub :send_instructions!, :foo do
-          assert_equal @tokenable_model.send_confirm_instructions!, :foo
+          assert_equal @tokenable_model.send_confirm_instructions!('foo'), :foo
         end
       end
 
